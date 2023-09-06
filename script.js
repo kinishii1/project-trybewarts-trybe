b4e194699608911f0e4dd7558c9e8b0cb24e2684
Kin Ishii <kinishii97@gmail.com>
2023-09-07 01:11:38 +0900

    terminando formulario de informaçoes

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 const enterButton = document.querySelector("#enter-button");
 const agreementCheckbox = document.querySelector("#agreement");
 const submitButton = document.querySelector("#submit-btn");
-const counterElement = document.querySelector('#counter');
-const textarea = document.querySelector('#textarea');
+const counterElement = document.querySelector("#counter");
+const textarea = document.querySelector("#textarea");
+const inputName = document.querySelector("#input-name");
+const inputEmail = document.querySelector("#input-email");
+const inputLastName = document.querySelector("#input-lastname");
+const inputHouse = document.querySelector("#house");
+const inputFamily = document.querySelectorAll(
+  'input[type="radio"][name="family"]'
+);
+const inputTechnologies = document.querySelectorAll(
+  'input[type="checkbox"][class="subject"]'
+);
+const inputRates = document.querySelectorAll(
+  'input[type="radio"][name="rate"]'
+);
+const inputTextArea = document.querySelector("#textarea");
+
+const dataForm = document.querySelector('#form-data')
+const mainForm = document.querySelector('#evaluation-form')
 
 const validyLogin = () => {
   const email = document.querySelector("#enter-email").value;
@@ -13,7 +30,8 @@ const validyLogin = () => {
   window.alert("Email ou senha inválidos.");
 };
 
-
+submitButton.disabled = true;
+agreementCheckbox.checked = false
 
 function verifyAgreement() {
   if (agreementCheckbox.checked) {
@@ -23,10 +41,64 @@ function verifyAgreement() {
   }
 }
 
-textarea.addEventListener('input', () => {
+textarea.addEventListener("input", () => {
   const counter = 500 - textarea.value.length;
   counterElement.innerText = `${counter} / 500`;
 });
 
 agreementCheckbox.addEventListener("click", verifyAgreement);
 enterButton.addEventListener("click", validyLogin);
+
+const nomeElement = document.getElementById("nome");
+const emailElement = document.getElementById("email");
+const casaElement = document.getElementById("casa");
+const familiaElement = document.getElementById("familia");
+const materiasElement = document.getElementById("materias");
+const notaElement = document.getElementById("nota");
+const obsElement = document.getElementById("obs");
+
+const createRate = () => {
+  let rateVal = "";
+  inputRates.forEach((rate) => {
+    if (rate.checked) {
+      rateVal = rate.value;
+    }
+  });
+  notaElement.innerText = `Avaliação: ${rateVal}`;
+};
+
+const createMessage = () => {
+  obsElement.innerText = `Observações: ${inputTextArea.value}`;
+};
+
+const createTechnologies = () => {
+  const tecs = [];
+  inputTechnologies.forEach((tec) => {
+    if (tec.checked) {
+      tecs.push(tec.value);
+    }
+  });
+  materiasElement.innerText = `Matérias: ${tecs.join(", ")}`;
+};
+
+const createFinalDocument = () => {
+  nomeElement.innerText = `Nome: ${inputName.value} ${inputLastName.value}`;
+  emailElement.innerText = `Email: ${inputEmail.value}`;
+  casaElement.innerText = `Casa: ${inputHouse.value}`;
+
+  inputFamily.forEach((family) => {
+    if (family.checked) {
+      const result = family.value;
+      familiaElement.innerText = `Família: ${result}`;
+    }
+    createTechnologies();
+    createRate();
+    createMessage();
+
+    mainForm.style.display = 'none'
+    dataForm.style.display = 'block'
+
+  });
+};
+
+submitButton.addEventListener("click", createFinalDocument);
